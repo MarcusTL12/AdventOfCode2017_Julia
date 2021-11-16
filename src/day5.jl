6d4341022a8a3eb301eb382f7291d4026b3efb34
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-11-16 19:43:36 +0100

    Day 5

--- src/day5.jl
+++ src/day5.jl
@@ -1,4 +1,38 @@
 
 function part1()
-    
+    jumps = open("input/day5/input") do io
+        parse.(Int, eachline(io))
+    end
+
+    i = 1
+    j = 0
+    while i in eachindex(jumps)
+        jump = jumps[i]
+        jumps[i] += 1
+        i += jump
+        j += 1
+    end
+
+    j
+end
+
+function part2()
+    jumps = open("input/day5/input") do io
+        parse.(Int, eachline(io))
+    end
+
+    i = 1
+    j = 0
+    while i in eachindex(jumps)
+        jump = jumps[i]
+        jumps[i] += if jump >= 3
+            -1
+        else
+            1
+        end
+        i += jump
+        j += 1
+    end
+
+    j
 end
