f49410cc3c5ab115b53bcbb3955fea35f2f4e729
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-11-16 16:06:01 +0100

    Day2

---
+++ src/day2.jl
@@ -0,0 +1,26 @@
+
+function part1()
+    open("input/day2/input") do io
+        -sum(-(extrema(s -> parse(Int, s), split(l))...) for l in eachline(io))
+    end
+end
+
+function part2()
+    open("input/day2/input") do io
+        sum(begin
+            nums = [parse(Int, s) for s in split(l)]
+
+            res = 0
+
+            for i in 1:length(nums) - 1, j in i + 1:length(nums)
+                q, r = divrem(reverse(minmax(nums[i], nums[j]))...)
+
+                if r == 0
+                    res = q
+                end
+            end
+
+            res
+        end for l in eachline(io))
+    end
+end
