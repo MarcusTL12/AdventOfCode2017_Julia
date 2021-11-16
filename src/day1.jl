a738a7d638bfe329da2b895bac88d6138c147531
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-11-16 15:33:47 +0100

    Day1

--- src/day1.jl
+++ src/day1.jl
@@ -1,4 +1,22 @@
 
 function part1()
-    println("Hei!")
+    inp = open(readline, "input/day1/input")
+
+    sum(x == y ? x - '0' : 0 for (x, y) in zip(inp, Iterators.drop(inp, 1))) +
+    (first(inp) == last(inp) ? first(inp) - '0' : 0)
+end
+
+function part2()
+    inp = open(readline, "input/day1/input")
+
+    nums = [c - '0' for c in inp]
+
+    sum(begin
+        opposite_index = (i - 1 + length(nums) ÷ 2) % length(nums) + 1
+        if nums[i] == nums[opposite_index]
+            nums[i]
+        else
+            0
+        end
+    end for i in eachindex(nums))
 end
