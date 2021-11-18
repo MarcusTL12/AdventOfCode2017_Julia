1b501b8f6360c369828ceebf20c19dd4877429bb
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-11-18 22:34:38 +0100

    Day9

--- src/day9.jl
+++ src/day9.jl
@@ -1,10 +1,59 @@
 
 function part1()
-    inp = "{{<!>},{<!>},{<!>},{<a>}}"
+    inp = readline("input/day9/input")
 
     iter = Iterators.Stateful(inp)
 
-    while (c = popfirst!(iter); !isnothing(c))
-        println(c)
+    garbage = false
+    depth = 0
+
+    score = 0
+
+    while !isempty(iter)
+        c = popfirst!(iter)
+
+        if garbage
+            if c == '!'
+                popfirst!(iter)
+            elseif c == '>'
+                garbage = false
+            end
+        elseif c == '{'
+            depth += 1
+            score += depth
+        elseif c == '}'
+            depth -= 1
+        elseif c == '<'
+            garbage = true
+        end
     end
+
+    score
+end
+
+function part2()
+    inp = readline("input/day9/input")
+
+    iter = Iterators.Stateful(inp)
+
+    garbage = false
+    amt_garbage = 0
+
+    while !isempty(iter)
+        c = popfirst!(iter)
+
+        if garbage
+            if c == '!'
+                popfirst!(iter)
+            elseif c == '>'
+                garbage = false
+            else
+                amt_garbage += 1
+            end
+        elseif c == '<'
+            garbage = true
+        end
+    end
+
+    amt_garbage
 end
